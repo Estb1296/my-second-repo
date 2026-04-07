7cb14e1862a7f56b04d3baa927b5daa9dbb5d341
ezrateshale <ezra.teshale12@gmail.com>
2026-04-07 15:52:33 -0500

    feat: Print something else in the main method- commit and push

--- src/main/java/com/pluralsight/HelloWorld.java
+++ src/main/java/com/pluralsight/HelloWorld.java
@@ -4,5 +4,6 @@ public class HelloWorld {
     public static void main(String[] args){
         System.out.println("Activity");
         System.out.println("Done some more printing");
+        System.out.println("Here");
     }
 }
