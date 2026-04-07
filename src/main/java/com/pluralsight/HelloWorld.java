3d8aca9564951852f179db75d2384994c921bef9
ezrateshale <ezra.teshale12@gmail.com>
2026-04-07 15:50:20 -0500

    feat: Create a main method - commit and push

--- src/main/java/com/pluralsight/HelloWorld.java
+++ src/main/java/com/pluralsight/HelloWorld.java
@@ -1,7 +1,7 @@
 package com.pluralsight;
 
 public class HelloWorld {
-    public void main(String[] args){
-
+    public static void main(String[] args){
+        System.out.println("Activity");
     }
 }
